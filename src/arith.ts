baab7b857ddf0178a1a8cc67884dc61ba0c72f2e
Eoin Davey <eoind@vey.ie>
2019-11-11 02:04:51 +0000

    Add Context Recorder interface + passthrough
    Add Context Recorder interface to allow parser to record it's state as
    it attempts to parse an input. This is put in place to allow for syntax
    error reporting. The interface is made abstract and public to also allow
    for custom tracers to be implemented.
    
    Also implement a large refactor to simplify generated code. Lot's of
    functionality pulled out into generic functions.

--- src/arith.ts
+++ src/arith.ts
@@ -1,8 +1,8 @@
 // Arithmetic parser example
 
 /* 
- * SUM  ::= head=FAC tail={op={'\+' | '-'} sm=FAC}*;
- * FAC  ::= head=ATOM tail={ op={'\*' | '/'} at=ATOM }*;
+ * SUM  ::= head=FAC tail={op='\+|-' sm=FAC}*;
+ * FAC  ::= head=ATOM tail={op='\*|/' at=ATOM }*;
  * ATOM ::= val=INT | '\(' val=SUM '\)';
  * INT  ::= val='[0-9]+';
  */
@@ -21,11 +21,11 @@ export enum ASTKinds {
     StrMatch,
     Int,
     Atom_1, Atom_2,
-    Fac, Fac$1, Fac$1$1,
-    Sum, Sum$1, Sum$1$1,
+    Fac, Fac$1,
+    Sum, Sum$1, 
 }
 
-export type ASTNode = Int | Atom | Fac | Sum;
+export type ASTNode = StrMatch | Int | Atom | Fac | Fac$1 | Sum | Sum$1;
 
 export class StrMatch implements ASTNodeIntf {
     kind: ASTKinds.StrMatch = ASTKinds.StrMatch;
@@ -85,15 +85,13 @@ export class Fac implements ASTNodeIntf, Visitable {
 
 export class Fac$1 implements ASTNodeIntf {
     kind : ASTKinds.Fac$1 = ASTKinds.Fac$1;
-    op: Fac$1$1;
+    op: StrMatch;
     at: Atom;
-    constructor(op : Fac$1$1, at : Atom){
+    constructor(op : StrMatch, at : Atom){
         this.op = op;
         this.at = at;
     }
 }
-
-export type Fac$1$1 = StrMatch | StrMatch;
 
 export class Sum implements ASTNodeIntf, Visitable {
     kind : ASTKinds.Sum = ASTKinds.Sum;
@@ -110,15 +108,17 @@ export class Sum implements ASTNodeIntf, Visitable {
 
 export class Sum$1 implements ASTNodeIntf {
     kind : ASTKinds.Sum$1 = ASTKinds.Sum$1;
-    op: Sum$1$1;
+    op: StrMatch;
     sm: Fac;
-    constructor(op : Sum$1$1, sm : Fac){
+    constructor(op : StrMatch, sm : Fac){
         this.op = op;
         this.sm = sm;
     }
 }
 
-export type Sum$1$1 = StrMatch | StrMatch;
+interface ContextRecorder {
+    record(pos: number, result: Nullable<ASTNode>) : void;
+}
 
 export class Parser {
     private pos : number = 0;
@@ -154,125 +154,132 @@ export class Parser {
         return null;
     }
 
-    private regexAccept(match : string) : Nullable<StrMatch> {
-        var reg = new RegExp(match, 'y');
+    private regexAccept(match : string, cr? : ContextRecorder) : Nullable<StrMatch> {
+        return this.run(() => {
+            var reg = new RegExp(match, 'y');
+            reg.lastIndex = this.mark();
+            const res = reg.exec(this.input);
+            if(res){
+                this.pos = reg.lastIndex;
+                return new StrMatch(res[0]);
+            }
+            return null;
+        }, cr);
+    }
+
+    private run<T extends ASTNode>(fn : ()=> Nullable<T>,
+        cr : ContextRecorder | undefined) : Nullable<T> {
         const mrk = this.mark();
-        reg.lastIndex = mrk;
-        const res = reg.exec(this.input);
-        if(res){
-            this.pos = reg.lastIndex;
-            return new StrMatch(res[0]);
-        }
+        const res = fn();
+        if(cr)
+            cr.record(mrk, res);
+        if(res)
+            return res;
         this.reset(mrk);
+        return null
+    }
+
+    private choice<T extends ASTNode>(cr : ContextRecorder | undefined,
+        fns : (() => Nullable<T>)[]) : Nullable<T> {
+        for(let f of fns){
+            const res = this.run(f, cr);
+            if(res)
+                return res;
+        }
         return null;
     }
 
     parse = this.matchSum;
 
-    matchInt() : Nullable<Int> {
-        const val = this.regexAccept(String.raw`[0-9]+`);
-        if(val)
-            return new Int(val);
-        return null;
-    }
-
-    matchAtom() : Nullable<Atom> {
-        {
-            const x = this.matchInt();
-            if(x)
-                return new Atom_1(x);
-        }
-        {
-            const mrk = this.mark();
-            let $1 : Nullable<StrMatch>;
-            let val : Nullable<Sum>;
-            let $2 : Nullable<StrMatch>;
-            if(($1 = this.regexAccept(String.raw`\(`))
-                && (val = this.matchSum())
-                && ($2 = this.regexAccept(String.raw`\)`)))
-                return new Atom_2(val);
-        }
-        return null;
-    }
-
-    matchFac$1$1() : Nullable<Fac$1$1> {
-        {
-            const res = this.regexAccept(String.raw`\*`);
-            if(res)
+    matchInt(cr? : ContextRecorder) : Nullable<Int> {
+        return this.choice<Int>(cr, [
+            ()=> {
+                let val : Nullable<StrMatch> = null;
+                let res : Nullable<Int> = null;
+                if((val = this.regexAccept(String.raw`[0-9]+`, cr)))
+                    res = new Int(val);
                 return res;
-        }
-        {
-            const res = this.regexAccept(String.raw`/`);
-            if(res)
+            }
+        ]);
+    }
+
+    matchAtom(cr? : ContextRecorder) : Nullable<Atom> {
+        return this.choice<Atom>(cr, [
+            () => {
+                let x : Nullable<Int>;
+                let res : Nullable<Atom> = null;
+                if((x = this.matchInt(cr)))
+                    res = new Atom_1(x);
                 return res;
-        }
-        return null;
-    }
-
-    matchFac$1() : Nullable<Fac$1> {
-        {
-            const mrk = this.mark();
-            let op : Nullable<Fac$1$1>;
-            let at : Nullable<Atom>;
-            if((op = this.matchFac$1$1())
-                && (at = this.matchAtom()))
-                return new Fac$1(op, at);
-            this.reset(mrk);
-        }
-        return null;
-    }
-
-    matchFac() : Nullable<Fac> {
-        {
-            const mrk = this.mark();
-            let head : Nullable<Atom>;
-            let tail : Nullable<Fac$1[]>;
-            if((head = this.matchAtom())
-                && (tail = this.loop<Fac$1>(() => this.matchFac$1(), true)))
-                return new Fac(head, tail);
-            this.reset(mrk);
-        }
-        return null;
-    }
-
-    matchSum$1$1() : Nullable<Sum$1$1> {
-        {
-            const res = this.regexAccept(String.raw`\+`);
-            if(res)
+            },
+            () => {
+                let $1 : Nullable<StrMatch>;
+                let val : Nullable<Sum>;
+                let $2 : Nullable<StrMatch>;
+                let res : Nullable<Atom> = null;
+                if(($1 = this.regexAccept(String.raw`\(`, cr))
+                    && (val = this.matchSum(cr))
+                    && ($2 = this.regexAccept(String.raw`\)`, cr)))
+                    res = new Atom_2(val);
                 return res;
-        }
-        {
-            const res = this.regexAccept(String.raw`-`);
-            if(res)
+            }
+        ]);
+    }
+
+    matchFac$1(cr? : ContextRecorder) : Nullable<Fac$1> {
+        return this.choice<Fac$1>(cr, [
+            () => {
+                let op : Nullable<StrMatch>;
+                let at : Nullable<Atom>;
+                let res : Nullable<Fac$1> = null;
+                if((op = this.regexAccept(String.raw`\*|/`, cr))
+                    && (at = this.matchAtom()))
+                    res = new Fac$1(op, at);
                 return res;
-        }
-        return null;
+            },
+        ]);
     }
 
-    matchSum$1() : Nullable<Sum$1> {
-        {
-            const mrk = this.mark();
-            let op : Nullable<Sum$1$1>;
-            let sm : Nullable<Fac>;
-            if((op = this.matchSum$1$1())
-                && (sm = this.matchFac()))
-                return new Sum$1(op, sm);
-            this.reset(mrk);
-        }
-        return null;
+    matchFac(cr? : ContextRecorder) : Nullable<Fac> {
+        return this.choice<Fac>(cr, [
+            () => {
+                let head : Nullable<Atom>;
+                let tail : Nullable<Fac$1[]>;
+                let res : Nullable<Fac> = null;
+                if((head = this.matchAtom())
+                    && (tail = this.loop<Fac$1>(() => this.matchFac$1(cr), true)))
+                    res = new Fac(head, tail);
+                return res;
+            }
+        ]);
     }
 
-    matchSum() : Nullable<Sum> {
-        {
-            const mrk = this.mark();
-            let head : Nullable<Fac>;
-            let tail : Nullable<Sum$1[]>;
-            if((head = this.matchFac())
-                && (tail = this.loop<Sum$1>(() => this.matchSum$1(), true)))
-                return new Sum(head, tail);
-            this.reset(mrk);
-        }
-        return null;
+    matchSum$1(cr? : ContextRecorder) : Nullable<Sum$1> {
+        return this.choice<Sum$1>(cr, [
+            () => {
+                let op : Nullable<StrMatch>;
+                let sm : Nullable<Fac>;
+                let res : Nullable<Sum$1> = null;
+                if((op = this.regexAccept(String.raw`\+|-`, cr))
+                    && (sm = this.matchFac(cr)))
+                    res = new Sum$1(op, sm);
+                return res;
+            },
+        ]);
+    }
+
+    matchSum(cr? : ContextRecorder) : Nullable<Sum> {
+        return this.choice<Sum>(cr, [
+            () => {
+                let head : Nullable<Fac>;
+                let tail : Nullable<Sum$1[]>;
+                let res : Nullable<Sum> = null;
+                if((head = this.matchFac(cr))
+                    && (tail = this.loop<Sum$1>(() => this.matchSum$1(cr), true)))
+                    res = new Sum(head, tail);
+                return res;
+            }, 
+        ]);
     }
 
     /* 
