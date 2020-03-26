72319c2f21ec5f0002c1c5406a3600baab64b56e
eoin <eoind@vey.ie>
2020-03-26 11:35:03 +0000

    Add more parser tests

--- src/test/gen.test.ts
+++ src/test/gen.test.ts
@@ -7,10 +7,13 @@ test("Parser Test", () => {
             inp: "rule := 'regex'"
         },
         {
+            inp: String.raw`rule := 'string \' with \' quote \' inside \' \' \''`
+        },
+        {
             inp: "rule := 'regex1' | 'regex2' | 'regex3'"
         },
         {
-            inp: "rule := named='regex1' | 'regex2' | named_again='regex3'"
+            inp: "rule := named='regex1' | 'regex2' | named_again  =  'regex3'"
         },
         {
             inp: "rule := named=rule_ref | 'regex'"
@@ -34,18 +37,43 @@ test("Parser Test", () => {
             rule_one := 'regex'
             rule_two := rule_one .test = boolean { return true; }`
         },
+        {
+            inp: `---HEADER---
+            rule_one := named=rule_ref
+                      .property = type { return function(); }
+                      | unnamed
+                      .prop = boolean { return true; }
+            rule_two := 'regex1'
+            rule_three := rule_one | capture=rule_two`
+        },
+        {
+            inp: "rule :=",
+            expmatches: ["[a-zA-Z_]+", "\\&|!", "\\'", "{", "@"]
+        },
+        {
+            inp: "rule := 'unterminated",
+            expmatches: ["\\'"]
+        },
+        {
+            inp: "rule := 'unmatched-op' | ",
+            expmatches: ["[a-zA-Z_]+", "\\&|!", "\\'", "{", "@"]
+        },
+        {
+            inp: "rule := 'can\\'t repeat @ special rule' @*",
+            expmatches: ["[a-zA-Z_]+", "\\&|!", "\\'", "{", "@", "\\.", "\\|"]
+        }
     ];
     for(const tc of tcs) {
         const res = parse(tc.inp);
         if (res.err !== null) {
             expect(tc.expmatches).not.toBeNull();
             expect(tc.expmatches).toEqual(res.err.expmatches);
-            return;
+            continue;
         }
         if (tc.expmatches !== undefined) {
             expect(res.err).not.toBeNull();
             expect(tc.expmatches).toEqual(res.err!.expmatches);
-            return;
+            continue;
         }
         expect(res.ast).not.toBeNull();
     }
