0e158de420c4580cee19141510a713dcefbe9051
eoin <eoind@vey.ie>
2020-09-23 21:56:56 +0100

    Fix linting errors

--- src/test/setanta_test/run.test.ts
+++ src/test/setanta_test/run.test.ts
@@ -4,12 +4,12 @@ import { Interpreter } from "setanta/node_build/i10r";
 
 test("Run simple program", async () => {
     // Calculate 21 fibonacci numbers
-    const fib_prog = `fib := [0, 1]
+    const fibProg = `fib := [0, 1]
 le i idir (0, 20)
     fib += [fib[fad@fib - 2] + fib[fad@fib - 1]]
 res := fib[fad@fib - 1]
 `;
-    const res = parse(fib_prog);
+    const res = parse(fibProg);
     expect(res.err).toBeNull();
     expect(res.ast).not.toBeNull();
     const i = new Interpreter();
