56cc1f3e0cf0b0e0e5d39bd1583a04074a24376b
Eoin Davey <eoind@vey.ie>
2024-11-09 01:58:16 +0000

    Update to new ESLint version

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import tslint from "typescript-eslint";
+import jslint from "@eslint/js";
+
+export default [
+    {
+        ignores: ["**/node_modules", "**/tsbuild", "src/**/parser.ts", "src/meta.ts"],
+    },
+    jslint.configs.recommended,
+    ...tslint.configs.strict,
+    {
+        rules: {
+            eqeqeq: ["error", "always"],
+            "no-promise-executor-return": "error",
+            "no-template-curly-in-string": "error",
+
+            "no-else-return": ["error", {
+                allowElseIf: false,
+            }],
+
+            "no-eval": "error",
+            "no-implied-eval": "error",
+            "no-loop-func": "error",
+            "no-useless-concat": "error",
+            "no-shadow": "error",
+
+            "brace-style": ["warn", "1tbs", {
+                allowSingleLine: true,
+            }],
+
+            "comma-spacing": "warn",
+            "key-spacing": "warn",
+            semi: ["error", "always"],
+            "space-infix-ops": "warn",
+
+            "sort-imports": ["warn", {
+                ignoreCase: false,
+                ignoreMemberSort: false,
+                ignoreDeclarationSort: true,
+                memberSyntaxSortOrder: ["none", "single", "multiple", "all"],
+                allowSeparatedGroups: false,
+            }],
+
+            "arrow-spacing": "warn",
+            "comma-dangle": ["warn", "always-multiline"],
+            "no-var": "error",
+            "prefer-const": "warn",
+        },
+    },
+    {
+        files: ["**/*.test.ts"],
+        rules: {
+            "@typescript-eslint/no-non-null-assertion": "off",
+        },
+    },
+];
