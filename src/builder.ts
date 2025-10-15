9301521c193228f62bb3b78e17525f154d9d69d9
Eoin Davey <eoind@vey.ie>
2025-10-15 23:39:14 +0100

    Make new naming algorithm match old

--- src/builder.ts
+++ src/builder.ts
@@ -3,7 +3,6 @@ import * as model from './model';
 
 export class ModelBuilder {
     private input: string;
-    private subExprCounter: number = 0;
 
     constructor(input: string) {
         this.input = input;
@@ -16,25 +15,23 @@ export class ModelBuilder {
     }
 
     private buildRule(ruleDef: ast.RULEDEF): model.Rule {
-        this.subExprCounter = 0; // Reset for each top-level rule
         const definition = this.buildMatchDisjunction(ruleDef.rule, ruleDef.name, ruleDef.name);
         return new model.Rule(ruleDef.name, definition, ruleDef.namestart);
     }
 
-    private buildMatchDisjunction(rule: ast.RULE, parentName: string, topLevelRuleName: string): model.MatchDisjunction {
-        if (rule.list.length === 1) {
-            const alternatives = [this.buildMatchSequence(rule.list[0], parentName, topLevelRuleName)];
-            return new model.MatchDisjunction(alternatives);
-        }
+    private buildMatchDisjunction(rule: ast.RULE, parentName: string, namingParent: string): model.MatchDisjunction {
+        let subExprCounter = 0;
+        const getSubExprId = () => subExprCounter++;
+
         const alternatives = rule.list.map((alt, i) => {
-            const altName = `${parentName}_${i + 1}`;
-            return this.buildMatchSequence(alt, altName, topLevelRuleName);
+            const altName = rule.list.length === 1 ? parentName : `${parentName}_${i + 1}`;
+            return this.buildMatchSequence(alt, altName, namingParent, getSubExprId);
         });
         return new model.MatchDisjunction(alternatives);
     }
 
-    private buildMatchSequence(alt: ast.ALT, name: string, topLevelRuleName: string): model.MatchSequence {
-        const matches = alt.matches.map(matchSpec => this.buildMatchSpec(matchSpec, topLevelRuleName));
+    private buildMatchSequence(alt: ast.ALT, name: string, namingParent: string, getSubExprId: () => number): model.MatchSequence {
+        const matches = alt.matches.map(matchSpec => this.buildMatchSpec(matchSpec, namingParent, getSubExprId));
         const attributes = alt.attrs.map(attr => this.buildComputedAttribute(attr));
         return new model.MatchSequence(name, matches, attributes);
     }
@@ -45,19 +42,19 @@ export class ModelBuilder {
         return new model.ComputedAttribute(attr.name, type, code, attr.type.start);
     }
 
-    private buildMatchSpec(matchSpec: ast.MATCHSPEC, topLevelRuleName: string): model.MatchSpec {
+    private buildMatchSpec(matchSpec: ast.MATCHSPEC, namingParent: string, getSubExprId: () => number): model.MatchSpec {
         const name = matchSpec.named ? matchSpec.named.name : null;
-        const expression = this.buildMatchExpression(matchSpec.rule, topLevelRuleName);
+        const expression = this.buildMatchExpression(matchSpec.rule, namingParent, getSubExprId);
         return new model.MatchSpec(name, expression);
     }
 
-    private buildMatchExpression(match: ast.MATCH, topLevelRuleName: string): model.MatchExpression {
+    private buildMatchExpression(match: ast.MATCH, namingParent: string, getSubExprId: () => number): model.MatchExpression {
         if (match.kind === ast.ASTKinds.SPECIAL) {
             return new model.SpecialMatch();
         }
 
         const postop = match;
-        const preExpression = this.buildPrefixExpression(postop.pre, topLevelRuleName);
+        const preExpression = this.buildPrefixExpression(postop.pre, namingParent, getSubExprId);
 
         if (!postop.op) {
             return preExpression;
@@ -90,8 +87,8 @@ export class ModelBuilder {
         return new model.PostfixExpression(preExpression, op, postop.pre.start);
     }
 
-    private buildPrefixExpression(preop: ast.PREOP, topLevelRuleName: string): model.MatchExpression {
-        const atomExpression = this.buildAtom(preop.at, topLevelRuleName);
+    private buildPrefixExpression(preop: ast.PREOP, namingParent: string, getSubExprId: () => number): model.MatchExpression {
+        const atomExpression = this.buildAtom(preop.at, namingParent, getSubExprId);
 
         if (!preop.op) {
             return atomExpression;
@@ -100,15 +97,17 @@ export class ModelBuilder {
         return new model.PrefixExpression(atomExpression, preop.op as model.PrefixOperator, preop.start);
     }
 
-    private buildAtom(atom: ast.ATOM, topLevelRuleName: string): model.MatchExpression {
+    private buildAtom(atom: ast.ATOM, namingParent: string, getSubExprId: () => number): model.MatchExpression {
         switch (atom.kind) {
             case ast.ASTKinds.ATOM_1: // RuleReference
                 return new model.RuleReference(atom.name, atom.start);
             case ast.ASTKinds.ATOM_2: // RegexLiteral
                 return new model.RegexLiteral(atom.match.val, atom.match.mods, atom.match.start);
             case ast.ASTKinds.ATOM_3: {
-                const subExprName = `${topLevelRuleName}_$${this.subExprCounter++}`;
-                const disjunction = this.buildMatchDisjunction(atom.sub, subExprName, topLevelRuleName);
+                const subExprName = `${namingParent}_$${getSubExprId()}`;
+                // For the recursion, the new naming parent is the name we just generated.
+                // This will create a new counter scope.
+                const disjunction = this.buildMatchDisjunction(atom.sub, subExprName, subExprName);
                 return new model.SubExpression(subExprName, disjunction);
             }
             case ast.ASTKinds.EOF: // EOF
